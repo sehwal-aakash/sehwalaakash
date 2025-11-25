41b2fb0d0ec3cc74839807a7291d36e765248baf
Aakash Sharma <sehwal.aakash@gmail.com>
2025-11-25 16:23:01 +0530

    Added the Client Images

--- src/components/sections/partnersMarquee.jsx
+++ src/components/sections/partnersMarquee.jsx
@@ -9,12 +9,85 @@ const PartnersMarquee = () => {
                 <div className="scroller">
                     <div className="scroller__inner">
                         <Marquee>
-                            <img src={"/images/client-logos/partner1.png"} alt="" />
-                            <img src={"/images/client-logos/partner2.png"} alt="" />
-                            <img src={"/images/client-logos/partner3.png"} alt="" />
-                            <img src={"/images/client-logos/partner4.png"} alt="" />
-                            <img src={"/images/client-logos/partner5.png"} alt="" />
-                            <img src={"/images/client-logos/partner1.png"} alt="" />
+                            <img src={"/images/client-logos/360-realtors-logo-image.jpg"} alt="360 Realtors Logo Image" />
+                            <img src={"/images/client-logos/adawati-logo.jpg"} alt="Adawati Logo Image" />
+                            <img src={"/images/client-logos/ahika-logo-image.jpg"} alt="Ahika Logo Image" />
+                            <img src={"/images/client-logos/amerdental-logo-image.jpg"} alt="Amerdental Logo Image" />
+                            <img src={"/images/client-logos/ana-anagulu-logo-image.jpg"} alt="Ana Anagulu Logo Image" />
+                            <img src={"/images/client-logos/aroma-by-design-logo.jpg"} alt="Aroma by Design Logo Image" />
+                            <img src={"/images/client-logos/artehouse-logo-image.jpg"} alt="Artehouse Logo Image" />
+                            <img src={"/images/client-logos/aswo-logo-image.jpg"} alt="Aswo Logo Image" />
+                            <img src={"/images/client-logos/aventon-logo.jpg"} alt="Aventon Logo Image" />
+                            <img src={"/images/client-logos/ayaati-logo-image.jpg"} alt="Ayaati Logo Image" />
+                            <img src={"/images/client-logos/bizstack-logo-img.jpg"} alt="Bizstack Logo Image" />
+                            <img src={"/images/client-logos/boka-logo-image.jpg"} alt="Boka Logo Image" />
+                            <img src={"/images/client-logos/bomani-logo-image.jpg"} alt="Drink Bomani Logo Image" />
+                            <img src={"/images/client-logos/branded-logo-image.jpg"} alt="BrandEd Logo Image" />
+                            <img src={"/images/client-logos/brave-paws-logo-image.jpg"} alt="Brave Paws Logo Image" />
+                            <img src={"/images/client-logos/cellbone-logo-image.jpg"} alt="Cellbone Logo Image" />
+                            <img src={"/images/client-logos/christine-logo-image.jpg"} alt="Christine Logo Image" />
+                            <img src={"/images/client-logos/collective-labs-logo-image.jpg"} alt="Collective Labs Logo Image" />
+                            <img src={"/images/client-logos/design-ink-india-logo-image.jpg"} alt="Design Ink India Logo Image" />
+                            <img src={"/images/client-logos/deva-swimming-logo-image.jpg"} alt="Deva Swimming Logo Image" />
+                            <img src={"/images/client-logos/dusaan-logo-image.jpg"} alt="Dusaan Logo Image" />
+                            <img src={"/images/client-logos/edge-clothing-logo-image.jpg"} alt="Edge Clothing Logo Image" />
+                            <img src={"/images/client-logos/everbloom-logo-image.jpg"} alt="Everbloom India Logo Image" />
+                            <img src={"/images/client-logos/exotic-logo-image.jpg"} alt="Exotic Logo Image" />
+                            <img src={"/images/client-logos/fabriclore-logo-image.jpg"} alt="Fabriclore Logo Image" />
+                            <img src={"/images/client-logos/foomer-logo-image.jpg"} alt="Foomer Logo Image" />
+                            <img src={"/images/client-logos/fusion-logo-image.jpg"} alt="Fusion Logo Image" />
+                            <img src={"/images/client-logos/giant-teddy-image.jpg"} alt="Giant Teddy Logo Image" />
+                            <img src={"/images/client-logos/gillori-logo-image.jpg"} alt="Gillori Logo Image" />
+                            <img src={"/images/client-logos/glamveda-logo-image.jpg"} alt="Glamveda Logo Image" />
+                            <img src={"/images/client-logos/gramiya-logo-image.jpg"} alt="Gramiya Logo Image" />
+                            <img src={"/images/client-logos/griffin-armament-logo-image.jpg"} alt="Griffin Armament Logo Image" />
+                            <img src={"/images/client-logos/habereindia-logo-image.jpg"} alt="Habere India Logo Image" />
+                            <img src={"/images/client-logos/handmatters-logo-image.jpg"} alt="Handmatters Logo Image" />
+                            <img src={"/images/client-logos/hidden-victoria-logo-image.jpg"} alt="Hidden Victoria Logo Image" />
+                            <img src={"/images/client-logos/ishqme-logo-image.jpg"} alt="Ishqme Logo Image" />
+                            <img src={"/images/client-logos/ixsound-logo-image.jpg"} alt="Ixsound Logo Image" />
+                            <img src={"/images/client-logos/jinaaya-logo-image.jpg"} alt="Jinaaya Logo Image" />
+                            <img src={"/images/client-logos/josera-logo-image.jpg"} alt="Josera Logo Image" />
+                            <img src={"/images/client-logos/kashmirvilla-logo-image.jpg"} alt="Kashmirvilla Logo Image" />
+                            <img src={"/images/client-logos/kepra-logo-image.jpg"} alt="Kepra Logo Image" />
+                            <img src={"/images/client-logos/kuchenkompane-logo-image.jpg"} alt="Kuchenkompane Logo Image" />
+                            <img src={"/images/client-logos/lighting-bolt-logo-image.jpg"} alt="Lightning Bolt Logo Image" />
+                            <img src={"/images/client-logos/little-hearts-desire-logo-image.jpg"} alt="Little Hearts Desire Logo Image" />
+                            <img src={"/images/client-logos/luse-essentials-logo-image.jpg"} alt="Luse Essentials Logo Image" />
+                            <img src={"/images/client-logos/main-hardware-logo-image.jpg"} alt="Main Hardware Logo Image" />
+                            <img src={"/images/client-logos/makkpress-logo-image.jpg"} alt="MakkPress Logo Image" />
+                            <img src={"/images/client-logos/manjeet-tatooz-logo-image.jpg"} alt="Manjeet Tatooz Logo Image" />
+                            <img src={"/images/client-logos/matrimonial-marriage-logo-image.jpg"} alt="Matrimonial Marriage Logo Image" />
+                            <img src={"/images/client-logos/mynee-mee-logo-image.jpg"} alt="Mynee Mee Logo Image" />
+                            <img src={"/images/client-logos/narre-logo-image.jpg"} alt="Naree Logo Image" />
+                            <img src={"/images/client-logos/newtral-logo-image.jpg"} alt="Newtral Logo Image" />
+                            <img src={"/images/client-logos/paisley-and-gray-logo-image.jpg"} alt="Paisley and Gray Logo Image" />
+                            <img src={"/images/client-logos/paramount-chikan-logo-image.jpg"} alt="Paramount Chikan Logo Image" />
+                            <img src={"/images/client-logos/pomcha-logo-image.jpg"} alt="Pomcha Logo Image" />
+                            <img src={"/images/client-logos/prabhubhakti-logo-image.jpg"} alt="Prabhu Bhakti Logo Image" />
+                            <img src={"/images/client-logos/pureclave-logo-image.jpg"} alt="Pureclave Logo Image" />
+                            <img src={"/images/client-logos/quilt-comfort-logo-image.jpg"} alt="Quilt Comfort Logo Image" />
+                            <img src={"/images/client-logos/rj-rijac-logo-image.jpg"} alt="RJ Rijac Logo Image" />
+                            <img src={"/images/client-logos/ryllz-essentials-logo.jpg"} alt="Ryllz Essentialls Logo Image" />
+                            <img src={"/images/client-logos/sam-gavriel-logo-image.jpg"} alt="Sam Gavriel Logo Image" />
+                            <img src={"/images/client-logos/scrapshala-logo-image.jpg"} alt="Scrapshala Logo Image" />
+                            <img src={"/images/client-logos/seerat-kaur-label-logo-image.jpg"} alt="Seerat Kaur Label Logo Image" />
+                            <img src={"/images/client-logos/shalini-india-logo-image.jpg"} alt="Shalini India Logo Image" />
+                            <img src={"/images/client-logos/shilpashastra-logo-image.jpg"} alt="Shilpashastra Logo Image" />
+                            <img src={"/images/client-logos/snazzyhunt-logo-image.jpg"} alt="Snazzyhunt Logo Image" />
+                            <img src={"/images/client-logos/stately-men-logo-image.jpg"} alt="Stately Men Logo Image" />
+                            <img src={"/images/client-logos/stylum-logo-image-2.jpg"} alt="Stylum Logo Image" />
+                            <img src={"/images/client-logos/stylum-logo-image.jpg"} alt="Stylum Logo Image" />
+                            <img src={"/images/client-logos/svedberga-logo-image.jpg"} alt="Svedberga Logo Image" />
+                            <img src={"/images/client-logos/swasti-clothing-logo.jpg"} alt="Swasti Clothing Logo Image" />
+                            <img src={"/images/client-logos/thujor-se-logo-image.jpg"} alt="Thujor Se Clothing Logo Image" />
+                            <img src={"/images/client-logos/trendia-logo-image.jpg"} alt="Trendia Logo Image" />
+                            <img src={"/images/client-logos/unisaif-logo-image.jpg"} alt="Unisaif Logo Image" />
+                            <img src={"/images/client-logos/vara-safety-logo-image.jpg"} alt="Vara Safety Logo Image" />
+                            <img src={"/images/client-logos/ved-habit-logo-image.jpg"} alt="Ved Habit Logo Image" />
+                            <img src={"/images/client-logos/wellversed-logo-image.jpg"} alt="Wellversed Logo Image" />
+                            <img src={"/images/client-logos/yash-gallery-logo-image.jpg"} alt="Yash Gallery Logo Image" />
+                            
                         </Marquee>
                     </div>
                 </div>
